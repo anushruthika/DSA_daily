e3bc02ab3c57b48f3254fab14e1d99b4f4f81064
Anushruthika <anushruthikae@gmail.com>
2025-05-09 17:05:37 +0530

    Update Rotate_String.py

--- Strings/Rotate_String.py
+++ Strings/Rotate_String.py
@@ -4,3 +4,8 @@ class Solution:
             if s[i:]+s[:i] == goal:
                 return True
         return False
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if goal in s+s and len(goal)==len(s):
+            return True
+        return False
