80dd713389deef794b709f543e43bd02006f4929
Anushruthika <anushruthikae@gmail.com>
2025-07-07 11:24:54 +0530

    Update min_stack.py

--- Stack/min_stack.py
+++ Stack/min_stack.py
@@ -1,3 +1,36 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.min_stack=[]
+        self.s=[]
+
+    def push(self, val):
+        self.s.insert(0,val)
+        if self.min_stack:
+            self.min_stack.insert(0,min(val,self.min_stack[0]))
+        else:
+            self.min_stack.insert(0,val)
+    def pop(self):
+        self.s.pop(0)
+        self.min_stack.pop(0)
+    def top(self):
+        return self.s[0]
+        
+
+    def getMin(self):
+        return self.min_stack[0]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+
 class MinStack:
 
     def __init__(self):
@@ -20,6 +53,7 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min_stack[-1]
+        
 
 
 # Your MinStack object will be instantiated and called as such:
