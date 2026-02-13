c1bfb2a1c96f7169ce87a1dcaec256aeb0f37bee
Anushruthika <anushruthikae@gmail.com>
2026-02-13 07:10:10 +0530

    Update and rename inorder.py to 1_inorder.py

--- Trees/1_inorder.py
+++ Trees/1_inorder.py
@@ -18,5 +18,32 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         self.inorder_rec(root,self.res)
         return self.res
+
+#### TEST CODE
+# Inorder : left - root - right
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.data=val
+        self.left = left
+        self.right=right
+def inorder(root,res):
+    if root == None:
+        return
+    inorder(root.left,res)
+    res.append(root.data)
+    inorder(root.right,res)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.right = Node(6)
+
+res = []
+inorder(root, res)
+
+for node in res:
+    print(node, end=" ")
         
         
