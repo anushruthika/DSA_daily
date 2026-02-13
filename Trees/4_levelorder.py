a8eb2c0e18503af0ebbbc8c2e92143c19b9456cd
Anushruthika <anushruthikae@gmail.com>
2026-02-13 07:48:28 +0530

    Update and rename levelorder.py to 4_levelorder.py

--- Trees/4_levelorder.py
+++ Trees/4_levelorder.py
@@ -24,3 +24,45 @@ class Solution:
         #     for j in i:
         #         c_res.append(j)
         return self.res
+
+# TEST CODE
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.data=val
+        self.left = left
+        self.right=right
+def levelorder(root,res,level):
+    if root:
+        if level>=len(res):
+            res.append([])
+        res[level].append(root.data)
+        levelorder(root.left,res,level+1,)
+        levelorder(root.right,res,level+1)
+    
+#      1
+#    /   \
+#   2     3
+#  / \   / \
+# 4   5 6   7
+# Preorder:
+# 1 2 4 5 3 6 7
+# Inorder:
+# 4 2 5 1 6 3 7
+# Postorder:
+# 4 5 2 6 7 3 1
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+res = []
+levelorder(root, res,0)
+
+for node in res:
+    print(node, end=" ")
+    
+        
+    
