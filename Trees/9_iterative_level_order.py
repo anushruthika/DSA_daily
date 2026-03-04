605d311691ad2bbdf750ccfafb79751cc922d5f5
Anushruthika <anushruthikae@gmail.com>
2026-03-04 10:37:39 +0530

    Update 9_iterative_level_order.py

--- Trees/9_iterative_level_order.py
+++ Trees/9_iterative_level_order.py
@@ -1,1 +1,27 @@
+# 102. Binary Tree Level Order Traversal
 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.res=[]
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root:
+            queue=[root]
+            while queue:
+                l=[]
+                for i in range(0,len(queue)):
+                    node = queue.pop(0)
+                    l.append(node.val)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+                self.res.append(l)
+        return self.res
