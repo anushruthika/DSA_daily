63ec32fd69d3a5150860ebb0690e97063ca4b49d
Anushruthika <anushruthikae@gmail.com>
2026-03-05 10:12:36 +0530

    Update 13_max_depth_recursion.py

--- Trees/13_max_depth_recursion.py
+++ Trees/13_max_depth_recursion.py
@@ -1,1 +1,19 @@
+# 104.Maximum Depth of a binary Tree
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rec(self,root):
+        if not root:
+            return 0
+        left_height=self.rec(root.left)
+        right_height=self.rec(root.right)
+        return max(left_height,right_height)+1
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.rec(root)
+
+        
