890acd024ad9f626e057bb4e42ceb6654055e629
Anushruthika <anushruthikae@gmail.com>
2026-03-10 10:59:46 +0530

    Create 19_vertical order_tree.py

---
+++ Trees/19_vertical order_tree.py
@@ -0,0 +1,45 @@
+# 987. Vertical Order Traversal of a Binary Tree
+
+class Solution:
+
+    def dfs(self, root, d:dict, cur):
+        row, col = cur
+
+        if root.left:
+            new_row, new_col = row + 1, col - 1
+            if new_col in d:
+                d[new_col].append((new_row, root.left.val))
+            else:
+                d[new_col]=[(new_row, root.left.val)]
+            self.dfs(root.left, d, (new_row, new_col))
+
+        if root.right:
+            new_row, new_col = row + 1, col + 1
+            if new_col in d:
+                d[new_col].append((new_row, root.right.val))
+            else:
+                d[new_col]=[(new_row, root.right.val)]
+            self.dfs(root.right, d, (new_row, new_col))
+
+    def verticalTraversal(self, root):
+        res=[]
+        if root:
+            d = {0:[(0,root.val)]} 
+            cur=(0,0)
+            self.dfs(root,d,cur)
+            for i in sorted(d.keys()):
+                # Python sorts tuples lexicographically: (row1, val1) < (row2, val2) 
+                # sort by row
+                # if row equal → sort by value
+                column = sorted(d[i])  # sort by row then value
+                
+                res.append([val for row, val in column])
+        return res
+
+        
+
+            
+            
+                
+
+        
