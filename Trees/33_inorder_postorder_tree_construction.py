5a4df1c73d105d115bfe487f6d3bc9e473a6b7fb
Anushruthika <anushruthikae@gmail.com>
2026-03-23 18:01:19 +0530

    Update 33_inorder_postorder_tree_construction.py

--- Trees/33_inorder_postorder_tree_construction.py
+++ Trees/33_inorder_postorder_tree_construction.py
@@ -3,6 +3,19 @@ class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         if not inorder or not postorder:
             return None
+        ind = inorder.index(postorder.pop())
+        root = TreeNode(inorder[ind])
+        print(inorder[ind+1:])
+        root.right = self.buildTree(inorder[ind+1:],postorder)
+        root.left = self.buildTree(inorder[:ind],postorder)
+        return root
+
+# approach 2
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not inorder or not postorder:
+            return None
         root=TreeNode(postorder[-1])
         index=inorder.index(postorder[-1])
         root.left=self.buildTree(inorder[:index],postorder[:index])
