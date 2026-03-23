25892e47ebdfbdc88bc0c61212fd86f9e1c0de53
Anushruthika <anushruthikae@gmail.com>
2026-03-23 19:12:37 +0530

    Create 35_flatten_BT.py

---
+++ Trees/35_flatten_BT.py
@@ -0,0 +1,32 @@
+# 114. Flatten Binary Tree to Linked List
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def serialize(self,root):
+        def dfs(root,res):
+            if root:
+                res.append(root.val)
+                dfs(root.left,res)
+                dfs(root.right,res)
+        res=[]
+        s=dfs(root,res)
+        return res
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        res = self.serialize(root)
+        if root:
+            root.left = None
+            cur = root
+            for i in res:
+                cur.right=TreeNode(i)
+            
+
+
+        
