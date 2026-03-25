16e494cbf52078ab7c0de0895228ad6b417fef59
Anushruthika <anushruthikae@gmail.com>
2026-03-26 00:36:31 +0530

    Create 004_reverse_odd_levels.py

---
+++ Trees/extras/004_reverse_odd_levels.py
@@ -0,0 +1,18 @@
+# 2415. Reverse Odd Levels of Binary Tree
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def dfs(node1, node2, level):
+            
+            if not node1 or not node2:
+                return
+            
+            if level % 2 != 0:
+                node1.val, node2.val = node2.val, node1.val
+            
+            dfs(node1.left, node2.right, level + 1)
+            dfs(node1.right, node2.left, level + 1)
+        
+        dfs(root.left, root.right, 1)
+
+        return root
