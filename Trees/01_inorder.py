1fef55c94230bfd26db1a0732dca83d5a067ddee
Anushruthika <anushruthikae@gmail.com>
2026-03-29 13:22:39 +0530

    Update 01_inorder.py

--- Trees/01_inorder.py
+++ Trees/01_inorder.py
@@ -1,13 +1,25 @@
 # 94. Binary Tree Inorder Traversal
 
-# Time: O(n), Space: O(n) (output) + O(h) recursion stack (worst O(n), best O(log n))
+# Time: O(n) => Each node is visited exactly once, 
+# Space: O(n) (output=self.res,Stores all n nodes ) + O(h) recursion stack (worst(skewed tree) O(n), best(balanced tree) O(log n))
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# recursion stack=> Recursion uses stack memory. All calls stay in memory until the deepest call returns.
+
+# eg: skewed Tree
+# 1
+#  \
+#   2
+#    \
+#     3
+#      \
+#       4
+
+# recursion stack: stores all these to return back to the root
+# call(1)
+#   → call(2)
+#     → call(3)
+#       → call(4)
+
 class Solution:
     def __init__(self):
         self.res=[]
