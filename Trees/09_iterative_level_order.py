62988c97e80cf05667cc10efc00ddf054862ec2c
Anushruthika <anushruthikae@gmail.com>
2026-03-29 13:47:56 +0530

    Update 09_iterative_level_order.py

--- Trees/09_iterative_level_order.py
+++ Trees/09_iterative_level_order.py
@@ -1,12 +1,42 @@
 # 102. Binary Tree Level Order Traversal
 
+# Time : O(n) Each node is processed exactly once => popleft() is O(1)
+# space: O(n)
+# Explanation: Queue: can hold up to one full level → O(n)
+# Output (self.res): stores all nodes → O(n) 
+# O(n) + O(n) = O(2n) = O(n) => In Big-O, we drop constants 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
+
+
+class Solution:
+    def __init__(self):
+        self.res=[]
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root:
+            # queue=[root]
+            queue = deque([root])
+            while queue:
+                l=[]
+                for i in range(0,len(queue)):
+                    node = queue.popleft() # popleft() is O(1)
+                    l.append(node.val)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+                self.res.append(l)
+        return self.res
+
+        
+### APPROACH 2=> without using deque 
+# Time : O(n^2) Each node is processed exactly once => pop(0) is O(n)
+# space: O(n)
+# Explanation: Queue: can hold up to one full level → O(n)
+# Output (self.res): stores all nodes → O(n) 
+# O(n) + O(n) = O(2n) = O(n) => In Big-O, we drop constants 
+
 class Solution:
     def __init__(self):
         self.res=[]
@@ -17,7 +47,7 @@ class Solution:
             while queue:
                 l=[]
                 for i in range(0,len(queue)):
-                    node = queue.pop(0)
+                    node = queue.pop(0) # popleft() is O(1) pop(0) is O(n)
                     l.append(node.val)
                     if node.left:
                         queue.append(node.left)
