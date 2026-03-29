3cbf749d42293eb9a2ca8134ea807d1a50adb17f
Anushruthika <anushruthikae@gmail.com>
2026-03-29 14:35:44 +0530

    Update 17_zigzag_or_spiral_tree.py

--- Trees/17_zigzag_or_spiral_tree.py
+++ Trees/17_zigzag_or_spiral_tree.py
@@ -1,5 +1,44 @@
 # 103. Binary Tree Zigzag Level Order Traversal
 
+# Time: O(n) => all nodes traversed once
+# Space: O(n) => queue + output storage
+
+from collections import deque
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        
+        if not root:
+            return res
+        
+        queue = deque([root])
+        sign = 1
+        
+        while queue:
+            level_size = len(queue)
+            l = deque()
+            
+            for _ in range(level_size):
+                cur = queue.popleft() # O(1)
+                
+                if sign == -1:
+                    l.appendleft(cur.val)   # O(1)
+                else:
+                    l.append(cur.val)       # O(1)
+                
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            
+            res.append(list(l))  # convert deque → list
+            sign *= -1
+        
+        return res
+
+## APPROACH 2: without using DEQUE
+# Time: O(n²) => pop(0) + insert(0) cause shifting
+# Space: O(n) => queue + output storage
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         res=[]
