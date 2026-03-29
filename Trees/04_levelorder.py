f068c225155d2034f576343730e3bd26fb993c9d
Anushruthika <anushruthikae@gmail.com>
2026-03-29 13:27:39 +0530

    Update 04_levelorder.py

--- Trees/04_levelorder.py
+++ Trees/04_levelorder.py
@@ -1,8 +1,7 @@
 # 102. Binary Tree Level Order Traversal
 
 # Time: O(n) => Each node is visited exactly once, 
-# Space: O(n) (output=self.res,Stores all n nodes as level list) + O(h) recursion stack (worst(skewed tree) 
-
+# Space: O(n) (output=self.res,Stores all n nodes ) + O(h) recursion stack (worst(skewed tree) O(n), best(balanced tree) O(log n))
 
 # Definition for a binary tree node.
 # class TreeNode:
