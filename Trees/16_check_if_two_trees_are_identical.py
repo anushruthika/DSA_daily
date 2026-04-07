ef4bc55e66a27cd40847787c212661363f65f87f
Anushruthika <anushruthikae@gmail.com>
2026-04-07 13:53:47 +0530

    Update 16_check_if_two_trees_are_identical.py

--- Trees/16_check_if_two_trees_are_identical.py
+++ Trees/16_check_if_two_trees_are_identical.py
@@ -1,4 +1,14 @@
 # 100. Same Tree
+# Time: O(n) => all nodes compared once
+# Space: O(h) (worst O(n) => skewed Tree, best O(log n) => balanced tree)
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
 
 # Time: O(n) => all nodes traversed once
 # Space: O(h) (worst O(n) => skewed Tree, best O(log n) => balanced tree)
