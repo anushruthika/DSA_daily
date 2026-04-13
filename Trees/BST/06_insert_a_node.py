f94d7daec3e69208ad10cde9471680262c1f7723
Anushruthika <anushruthikae@gmail.com>
2026-04-13 13:33:26 +0530

    Update 06_insert_a_node.py

--- Trees/BST/06_insert_a_node.py
+++ Trees/BST/06_insert_a_node.py
@@ -12,3 +12,24 @@ class Solution:
         if root.val > val:
             root.left = self.insertIntoBST(root.left, val)
         return root
+
+#iteration
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        cur = root
+        prev = None
+        while cur:
+            if cur.val < val:
+                prev = cur
+                cur = cur.right
+            else:
+                prev = cur
+                cur = cur.left
+                
+        if prev.val < val:
+            prev.right = TreeNode(val)
+        else:
+            prev.left = TreeNode(val)
+        return root
