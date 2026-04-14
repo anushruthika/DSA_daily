50371396a072bd1ced8a3fccc4ff823373161348
Anushruthika <anushruthikae@gmail.com>
2026-04-14 16:13:14 +0530

    Update 03_flood_fill.py

--- Graph/03_flood_fill.py
+++ Graph/03_flood_fill.py
@@ -20,4 +20,23 @@ class Solution:
                     image[r][c] = color
                     queue.append((r,c))
         return image
-      
+
+#####
+# DFS
+#####
+# Time: O(m * n) => each cell is visited at most once
+# Space: O(m * n) => recursion stack in worst case (grid fully connected), best O(1) to O(log n) depending on shape
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        org = image[sr][sc]
+        def flood_fill(x,y):
+            if (x < 0 or x >= len(image)) or (y < 0 or y >= len(image[0])) or (image[x][y] == color) or (image[x][y] != org): 
+                return
+            image[x][y] = color
+            flood_fill(x-1, y)
+            flood_fill(x+1, y)
+            flood_fill(x, y+1)
+            flood_fill(x, y-1)
+        if org!=color:
+            flood_fill(sr, sc)
+        return image
