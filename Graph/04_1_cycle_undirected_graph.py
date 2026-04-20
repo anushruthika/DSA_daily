ea8343da6fdc6810be2e53faa9e3b3b6c210ac2b
Anushruthika <anushruthikae@gmail.com>
2026-04-20 09:31:45 +0530

    Create 04_1_cycle_undirected_graph.py

---
+++ Graph/04_1_cycle_undirected_graph.py
@@ -0,0 +1,27 @@
+# Time: O(V + E) => building adjacency list takes O(E), and DFS visits each node and edge once
+# Space: O(V + E) => adjacency list stores all edges + visited array + recursion stack (worst case O(V))
+
+class Solution:
+	def isCycle(self, V, edges):
+		# step1: form adj list
+		adj = [[] for _ in range(V)]
+		for i,j in edges:
+		    adj[i].append(j)
+		    adj[j].append(i) 
+		print(adj)
+		visited = [False]*V
+		def isCycle(node,parent):
+		        visited[node] = True
+		        for path in adj[node]:
+		            if not visited[path]:
+    		            if isCycle(path,node):
+    		                return True
+		            elif path!=parent:
+		                return True
+		        return False
+		            
+	    for i in range(V):
+	        if not visited[i]:
+	            if isCycle(i,-1):
+	                return True
+	    return False
