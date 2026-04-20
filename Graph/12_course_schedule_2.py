8d53c23d62cdc540bcef196eb3aadc7fff8376d2
Anushruthika <anushruthikae@gmail.com>
2026-04-20 19:09:12 +0530

    Update 12_course_schedule_2.py

--- Graph/12_course_schedule_2.py
+++ Graph/12_course_schedule_2.py
@@ -3,10 +3,11 @@
 
 # 210. Course Schedule II
 
+# DFS
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adj = [[] for _ in range(numCourses)]
-        for pre,course in prerequisites:
+        for pre, course in prerequisites:
             adj[course].append(pre)
         visited = [False]*numCourses
         path = [False]*numCourses
@@ -28,3 +29,29 @@ class Solution:
                 if isCycle(i):
                     return []
         return res[::-1]
+
+# BFS:= Khans algorithm
+class Solution:
+    def findOrder(self, V: int, edges: List[List[int]]) -> List[int]:
+        adj = [[] for _ in range(V)]
+        indegree=[0]*V
+        for i,j in edges:
+            adj[j].append(i)
+            indegree[i]+=1
+        visited = [False]*V
+        stack = []
+        res=[]
+        for i in range(V):
+            if indegree[i] == 0:
+                stack.append(i)
+        while stack:
+            node = stack.pop()
+            res.append(node)
+            print(adj[node],node)
+            for path_node in adj[node]:
+                indegree[path_node]-=1
+                if indegree[path_node] == 0:
+                    stack.append(path_node)
+        if len(res)!=V:
+            return []
+        return res
