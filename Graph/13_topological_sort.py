a1382d781db60e53eab5244aa688cbdf10312937
Anushruthika <anushruthikae@gmail.com>
2026-04-20 10:37:36 +0530

    Update 13_topological_sort.py

--- Graph/13_topological_sort.py
+++ Graph/13_topological_sort.py
@@ -1,8 +1,11 @@
 # https://www.geeksforgeeks.org/problems/topological-sort/1
+# topological sorting => can be done only in DAG
+
+
+# DFS
 
 # Time: O(V + E) => building adjacency list takes O(E), and DFS visits each node and edge once
 # Space: O(V + E) => adjacency list stores all edges + visited array + recursion stack (worst case O(V))
-
 class Solution:
     def topoSort(self, V, edges):
         adj = [[] for _ in range(V)]
@@ -23,3 +26,33 @@ class Solution:
                 # print(visited,stack)
         # print(stack[::-1])
         return stack[::-1]
+
+# BFS = Khans algorithm - indegree instead of visited 
+# https://www.youtube.com/watch?v=73sneFXuTEg
+
+# Time: O(V + E) => building adjacency list takes O(E), and each node + edge is processed once in Kahn’s algorithm
+
+# Space: O(V + E) => adjacency list stores edges + indegree array + stack + result list
+class Solution:
+    def topoSort(self, V, edges):
+        adj = [[] for _ in range(V)]
+        indegree=[0]*V
+        for i,j in edges:
+            adj[i].append(j)
+            indegree[j]+=1
+        
+        # print(adj,indegree)
+        stack=[]
+        res=[]
+        for i in range(V):
+            if indegree[i] == 0:
+                stack.append(i)
+        while stack:
+            node = stack.pop()
+            res.append(node)
+            for path in adj[node]:
+                indegree[path]-=1
+                if indegree[path] == 0:
+                    stack.append(path)
+        return res
+
