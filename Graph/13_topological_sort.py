d6eeeee754d173948e148a7babb76f0bbebf4678
Anushruthika <anushruthikae@gmail.com>
2026-05-05 15:49:32 +0530

    Update 13_topological_sort.py

--- Graph/13_topological_sort.py
+++ Graph/13_topological_sort.py
@@ -1,6 +1,8 @@
 # https://www.geeksforgeeks.org/problems/topological-sort/1
 # topological sorting => can be done only in DAG
 
+# Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that for every directed edge u -> v, 
+# vertex u comes before v in the ordering.
 
 # DFS
 
