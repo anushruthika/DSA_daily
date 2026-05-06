f4feaac762b9b58216298c97caf81ba05cf24735
Anushruthika <anushruthikae@gmail.com>
2026-05-06 09:36:03 +0530

    Update 02_dijkstra_algorithm_PQ.py

--- Graph/shortest_path/02_dijkstra_algorithm_PQ.py
+++ Graph/shortest_path/02_dijkstra_algorithm_PQ.py
@@ -1,7 +1,15 @@
 # https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1
 
 # Time: O((V + E) log V)
+# 1=> O(logV) => heap operation to push/pop one V. O(VlogV) to push V vertex.
+# 2=> O(logV) => heap operation to push/pop one V. O(ElogV) to relax E edges
+# therefore, O(VlogV)+O(ElogV) = O((V+E)logV)
+
 # Space: O(V + E)
+# Adjacency list stores all edges → O(E)
+# - Distance array stores V nodes → O(V)
+# - Heap can hold up to V elements → O(V)
+# ⇒ Total: O(V + E)
 
 from typing import List
 from collections import deque
@@ -23,6 +31,7 @@ class Solution:
             if d > res[x]:
               continue
             for y,w in graph[x]:
+                # Edge relaxation: Can I reach y cheaper through x?
                 if res[y]>res[x]+w:
                     res[y] = res[x]+w
                     heapq.heappush(pq,(res[y],y))
