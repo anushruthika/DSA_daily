415596ca86c3097b2191e72179f002167ffcefb4
Anushruthika <anushruthikae@gmail.com>
2026-05-06 10:01:21 +0530

    Update 04_shortest_path_in_binary_matrix.py

--- Graph/shortest_path/04_shortest_path_in_binary_matrix.py
+++ Graph/shortest_path/04_shortest_path_in_binary_matrix.py
@@ -1,11 +1,21 @@
 # https://leetcode.com/problems/shortest-path-in-binary-matrix/submissions/1990716231/
 
-# Time: O(n^2)  
-#   Reason: Each cell in the n x n grid is visited at most once, and for each cell we check 8 directions (constant work).
-# Worst-case Time: O(n^2)  
-#   Reason: In the worst case (all cells are 0), BFS explores the entire grid before reaching the destination or concluding it's unreachable.
-# Space: O(n^2)  
-#   Reason: The queue can hold up to O(n^2) cells, and the visited set can also store up to O(n^2) cells.
+# Use Dijkstra's algorithm when graph edges have different (weighted) costs, to always find the minimum-distance path.
+
+# here, BFS (queue / deque) efficient
+# -------------------------------
+# 2️⃣ BFS (queue / deque)
+# -------------------------------
+
+# Time: O(n^2)
+# - Each cell visited at most once → O(n^2)
+# - Each has constant 8 neighbors
+# ⇒ O(n^2)
+
+# Space: O(n^2)
+# - Queue can hold up to n^2 cells
+# - Visited set stores up to n^2 cells
+# ⇒ O(n^2)
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
@@ -27,4 +37,36 @@ class Solution:
                     visited.add((nx,ny))
                     queue.append((nx,ny,d+1))
         return -1
-            
+
+# -------------------------------
+# 1️⃣ Dijkstra (heap / priority queue)
+# -------------------------------
+
+# Time: O(n^2 log n)
+# - Total nodes = n^2
+# - Each cell can be pushed into heap → O(n^2) pushes
+# - Each push/pop takes log(n^2) = log n
+# ⇒ Total: O(n^2 log n)
+
+# Space: O(n^2)
+# - Heap can store up to n^2 cells
+# - Grid used as visited
+# ⇒ O(n^2)
+import heapq
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if grid[0][0]!=0 or grid[n-1][n-1]!=0:
+            return -1
+        directions = [(0,1),(1,0),(0,-1),(-1,0),(1,-1),(1,1),(-1,1),(-1,-1)]
+        pq = [(1,0,0)]
+        while pq:
+            d,r,c = heapq.heappop(pq)
+            if r ==n-1 and c == n-1:
+                return d
+            for dr,dc in directions:
+                nr,nc = dr+r,dc+c
+                if 0<=nr<n and 0<=nc<n and grid[nr][nc] == 0:
+                    grid[nr][nc] = 1
+                    heapq.heappush(pq,(d+1,nr,nc))
+        return -1
