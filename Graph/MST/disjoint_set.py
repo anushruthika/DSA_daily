3529851bfff9c54c23ee04292eff7a8865d02a86
Anushruthika <anushruthikae@gmail.com>
2026-05-07 12:02:56 +0530

    Create disjoint_set.py

---
+++ Graph/MST/disjoint_set.py
@@ -0,0 +1,38 @@
+class DisjointSet:
+    
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+
+    # Find parent with path compression
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    # Union by rank
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+
+        if pu == pv:
+            return False   # already connected
+
+        if self.rank[pu] < self.rank[pv]:
+            self.parent[pu] = pv
+
+        elif self.rank[pv] < self.rank[pu]:
+            self.parent[pv] = pu
+
+        else:
+            self.parent[pv] = pu
+            self.rank[pu] += 1
+
+        return True
+        
+ds = DisjointSet(5)
+
+ds.union(0, 1)
+ds.union(1, 2)
+
+print(ds.find(2))   
