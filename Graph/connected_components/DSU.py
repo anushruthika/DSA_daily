ba91f614ebfa705535d5109dd085d364d1965403
Anushruthika <anushruthikae@gmail.com>
2026-05-10 11:12:18 +0530

    Update DSU.py

--- Graph/connected_components/DSU.py
+++ Graph/connected_components/DSU.py
@@ -36,3 +36,37 @@ ds.union(0, 1)
 ds.union(1, 2)
 
 print(ds.find(2))   
+
+
+
+
+class DSU:
+    
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1] * n
+
+    # Find with path compression
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    # Union by size
+    def union(self, u, v):
+
+        pu = self.find(u)
+        pv = self.find(v)
+
+        # already connected
+        if pu == pv:
+            return False
+
+        # attach smaller component to larger component
+        if self.size[pu] < self.size[pv]:
+            pu, pv = pv, pu
+
+        self.parent[pv] = pu
+        self.size[pu] += self.size[pv]
+
+        return True
