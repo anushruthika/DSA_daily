1b16a3318e2a252535895f7f93219b9f8f243729
Anushruthika <anushruthikae@gmail.com>
2026-05-12 18:49:25 +0530

    Create 09_longest_subarray_with_k.py

---
+++ arrays/easy/09_longest_subarray_with_k.py
@@ -0,0 +1,18 @@
+# Time  -> O(n) 
+# Space -> O(n) => dictionary
+
+class Solution:
+    def longestSubarray(self, arr, k):  
+        d = {}
+        s = 0
+        length = 0
+        for i in range(len(arr)):
+            s+=arr[i]
+            if s==k:
+                length = i+1
+            rem = s-k
+            if rem in d:
+                length = max(length,i+1-d[rem])
+            if s not in d:
+                d[s] = i+1
+        return length
