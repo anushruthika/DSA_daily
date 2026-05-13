e615c640af26790e971034b2fa5522093e0b828f
Anushruthika <anushruthikae@gmail.com>
2026-05-13 09:30:47 +0530

    Create 01_clone_graph.py

---
+++ Graph/dfs_bfs/01_clone_graph.py
@@ -0,0 +1,22 @@
+# 133. Clone Graph
+
+# TC : O(V+E):
+# Every node is visited once → O(V)
+# Every edge is traversed once → O(E)
+# SC : O(V)
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return
+        # Stores the copied values & acts like a visited array too
+        copied = {}
+        def dfs(node):
+            # check if already visited
+            if node in copied:
+                return copied[node]
+            copied[node] = Node(node.val,[])
+            for nei in node.neighbors:
+                copied[node].neighbors.append(dfs(nei))
+            return copied[node]
+        return dfs(node)
