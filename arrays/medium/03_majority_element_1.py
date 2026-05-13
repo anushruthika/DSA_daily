ed64fcf3972acd3ceb80f508061227a7faeb5885
Anushruthika <anushruthikae@gmail.com>
2026-05-13 19:34:01 +0530

    Create 03_majority_element_1.py

---
+++ arrays/medium/03_majority_element_1.py
@@ -0,0 +1,42 @@
+#  voting algorithm:
+# time : o(n),
+# space: o(1)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+        for i in nums:
+            if count ==0:
+                candidate = i
+            if candidate == i:
+                count+=1
+            else:
+                count -=1
+        return candidate
+
+
+# time : o(nlogn)- sorting,
+# space: o(1)
+# approach: sorted [1,1,1,2,2,2,2] nums[3] = 2 middle is the answer
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        return sorted(nums)[math.ceil(len(nums)//2)]
+
+
+# time : o(n),
+# space: o(n) to store dict
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        max_vote = float('-inf')
+        candidate = None
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i]+=1
+            if d[i]>max_vote:
+                max_vote = d[i]
+                candidate = i
+        return candidate
+
