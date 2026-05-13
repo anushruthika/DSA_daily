eee7cb60da9010d9cc4d8dccde092288e7ab0ef9
Anushruthika <anushruthikae@gmail.com>
2026-05-13 14:48:49 +0530

    Create 01_x01_graph_valid_tree.py

---
+++ Graph/topological_sort/01_x01_graph_valid_tree.py
@@ -0,0 +1,59 @@
+# gfg not working
+class Solution:
+
+    def isTree(self, n, m, edges):
+
+        # Time Complexity : O(V + E)
+        # Space Complexity: O(V)
+
+        # Tree must have exactly n-1 edges
+        if m != n - 1:
+            return 0
+
+        # Build adjacency list
+        adj = [[] for _ in range(n)]
+
+        for u, v in edges:
+
+            # Self loop
+            if u == v:
+                return 0
+
+            adj[u].append(v)
+            adj[v].append(u)
+
+        visited = [False] * n
+
+        # DFS function
+        def dfs(node, parent):
+
+            visited[node] = True
+
+            for nei in adj[node]:
+
+                # Ignore parent edge
+                if nei == parent:
+                    continue
+
+                # Cycle detected
+                if visited[nei]:
+                    return False
+
+                if not visited[nei]:
+
+                    if not dfs(nei, node):
+                        return False
+
+            return True
+
+        # Check cycle
+        if not dfs(0, -1):
+            return 0
+
+        # Check connectivity
+        for i in range(n):
+
+            if not visited[i]:
+                return 0
+
+        return 1
