647a0333caf1a1932e0b41b114162c370fe74897
Anushruthika <anushruthikae@gmail.com>
2026-05-18 10:10:16 +0530

    Create 02_maximum_consecutive_ones_3.py

---
+++ sliding_window__Two_pointer/02_maximum_consecutive_ones_3.py
@@ -0,0 +1,19 @@
+# 1004. Max Consecutive Ones III
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        i = 0
+        j = 0 
+        while j < len(nums):
+            if nums[j] ==0:
+                k-=1
+            if k<0:
+                if nums[i] ==0:
+                    k+=1
+                i+=1
+            j+=1
+        return j-i
+
+
