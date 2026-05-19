8a6e48086c1a52ddcd035f241911faf945449672
Anushruthika <anushruthikae@gmail.com>
2026-05-19 07:53:02 +0530

    Create 06_count_no_of_nice_subarrays.py

---
+++ sliding_window__Two_pointer/06_count_no_of_nice_subarrays.py
@@ -0,0 +1,23 @@
+# 1248. Count Number of Nice Subarrays
+
+
+class Solution:
+    def atmost(self,nums,k):
+        if k<0:
+            return 0
+        tracker=0
+        i=0
+        j=0
+        c =0
+        while j<len(nums):
+            if nums[j]%2!=0:
+                tracker+=1
+            while tracker >k:
+                if nums[i]%2!=0:
+                    tracker-=1
+                i+=1
+            c+=j-i+1
+            j+=1
+        return c
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        return self.atmost(nums,k) - self.atmost(nums,k-1)
