f0535139480c40aa3f933133949edb0f9ee38063
Anushruthika <anushruthikae@gmail.com>
2026-05-19 09:35:26 +0530

    Create 001_largest_positive.py

---
+++ arrays/extra/001_largest_positive.py
@@ -0,0 +1,19 @@
+# 2441. Largest Positive Integer That Exists With Its Negative
+
+
+# Time Complexity: O(n log n) (sorting)
+# Space Complexity: O(n)
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        s = set()
+        nums.sort()
+        max_now = -1
+        for num in nums:
+            if (-1*num) not in s:
+                s.add(num)
+            if -1*num in s:
+                max_now = num
+        return max_now
+
+
+        
