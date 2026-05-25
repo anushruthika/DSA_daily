53e809c9fe8c4f86cf972f39b05933db3a735ed4
Anushruthika <anushruthikae@gmail.com>
2026-05-25 10:51:47 +0530

    Update and rename min_stack.py to 006_min_stack.py

---
+++ Stack/006_min_stack.py
@@ -0,0 +1,31 @@
+# 155. Min Stack
+# TC: O(1) SC:O(n)
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(self.getMin(),val))
+            
+    def pop(self) -> None:
+        self.min_stack.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
