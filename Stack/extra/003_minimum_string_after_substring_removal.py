6d342adf0abc18a5440a13932ad2f02245da4907
Anushruthika <anushruthikae@gmail.com>
2026-05-25 14:39:17 +0530

    Create 003_minimum_string_after_substring_removal.py

---
+++ Stack/extra/003_minimum_string_after_substring_removal.py
@@ -0,0 +1,14 @@
+# 2696. Minimum String Length After Removing Substrings
+# TC: O(n) SC: O(n)
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+        for i in s:
+            if i =='B' and stack and stack[-1] == 'A':
+                    stack.pop()
+            elif i == 'D'and stack and stack[-1] == 'C':
+                    stack.pop()
+            else:
+                stack.append(i)
+        return len(stack)
+                
