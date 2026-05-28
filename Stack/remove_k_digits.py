f85ffd9cd03e9c907bff02e9cb298d44e7d3c1a3
Anushruthika <anushruthikae@gmail.com>
2026-05-28 10:47:18 +0530

    Update remove_k_digits.py

--- Stack/remove_k_digits.py
+++ Stack/remove_k_digits.py
@@ -1,3 +1,5 @@
+# 402. Remove K Digits
+# TC: O(n) SC: O(n)
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         stack=[]
@@ -6,7 +8,9 @@ class Solution:
                 stack.pop()
                 k-=1
             stack.append(i)
+        # edge case: num = "9" k = 1 res ="0" and not res= "9"
         stack=stack[:len(stack)-k]
+        # edge case: num = "12" k = 2 res = "0" and not res="" 
         res="".join(stack).lstrip("0") 
         return res if res else "0"
         
