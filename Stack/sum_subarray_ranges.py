3a417b55394850de75ab52148cc1546a5ea95640
Anushruthika <anushruthikae@gmail.com>
2026-05-28 10:20:11 +0530

    Update sum_subarray_ranges.py

--- Stack/sum_subarray_ranges.py
+++ Stack/sum_subarray_ranges.py
@@ -1,3 +1,89 @@
+# 2104. Sum of Subarray Ranges
+
+# TC: O(n)
+# SC: O(n)
+class Solution:
+    def nse(self, arr):
+        stack =[]
+        n = len(arr)
+        res = [n]*n
+        for i in range(n-1,-1,-1):
+            while stack and arr[stack[-1]]>=arr[i]:
+                stack.pop()
+            if stack and arr[stack[-1]]<arr[i]:
+                res[i] = stack[-1]
+            stack.append(i)
+        return res
+    def pse(self, arr):
+        stack =[]
+        n = len(arr)
+        res = [-1]*n
+        for i in range(n):
+            while stack and arr[stack[-1]]>arr[i]:
+                stack.pop()
+            if stack and arr[stack[-1]]<=arr[i]:
+                res[i] = stack[-1]
+            stack.append(i)
+        return res
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        nse = self.nse(arr)
+        pse = self.pse(arr)
+        tot = 0
+        for i in range(len(arr)):
+            # number of subarrays on left
+            left = i - pse[i]
+            # number of subarrays on right
+            right = nse[i] - i
+            tot += left * right * arr[i]
+        return tot
+    def nge(self, arr):
+        stack =[]
+        n = len(arr)
+        res = [n]*n
+        for i in range(n-1,-1,-1):
+            while stack and arr[stack[-1]]<=arr[i]:
+                stack.pop()
+            if stack and arr[stack[-1]]>arr[i]:
+                res[i] = stack[-1]
+            stack.append(i)
+        return res
+    def pge(self, arr):
+        stack =[]
+        n = len(arr)
+        res = [-1]*n
+        for i in range(n):
+            while stack and arr[stack[-1]]<arr[i]:
+                stack.pop()
+            if stack and arr[stack[-1]]>=arr[i]:
+                res[i] = stack[-1]
+            stack.append(i)
+        return res
+    def sumSubarrayMaxs(self, arr: List[int]) -> int:
+        nge = self.nge(arr)
+        pge = self.pge(arr)
+        tot = 0
+        for i in range(len(arr)):
+            # number of subarrays on left
+            left = i - pge[i]
+            # number of subarrays on right
+            right = nge[i] - i
+            tot += left * right * arr[i]
+        return tot 
+    def subArrayRanges(self, nums: List[int]) -> int:
+        return self.sumSubarrayMaxs(nums) - self.sumSubarrayMins(nums)
+
+
+
+
+
+
+##################################################################################################################################################
+
+
+
+
+
+
 class Solution(object):
 
     def sumSubarrayMax(self, arr):
