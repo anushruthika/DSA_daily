070d8f2a7bc76ebf1ffa1c25c3a7509b5e6a3159
Anushruthika <anushruthikae@gmail.com>
2026-05-28 10:01:15 +0530

    Update sum_subarray_mins.py

--- Stack/sum_subarray_mins.py
+++ Stack/sum_subarray_mins.py
@@ -1,3 +1,49 @@
+# TC: O(n)
+# SC: O(n)
+# 907. Sum of Subarray Minimums
+
+class Solution:
+    def nse(self, arr):
+        stack =[]
+        n = len(arr)
+        res = [n]*n
+        for i in range(n-1,-1,-1):
+            while stack and arr[stack[-1]]>=arr[i]:
+                stack.pop()
+            if stack and arr[stack[-1]]<arr[i]:
+                res[i] = stack[-1]
+            stack.append(i)
+        return res
+    def pse(self, arr):
+        stack =[]
+        n = len(arr)
+        res = [-1]*n
+        for i in range(n):
+            while stack and arr[stack[-1]]>arr[i]:
+                stack.pop()
+            if stack and arr[stack[-1]]<=arr[i]:
+                res[i] = stack[-1]
+            stack.append(i)
+        return res
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        nse = self.nse(arr)
+        pse = self.pse(arr)
+        tot = 0
+        MOD = 10**9 + 7
+
+        for i in range(len(arr)):
+
+            # number of subarrays on left
+            left = i - pse[i]
+
+            # number of subarrays on right
+            right = nse[i] - i
+
+            tot += left * right * arr[i]
+
+        return tot % MOD
+
+
 class Solution(object):
     def sumSubarrayMins(self, arr):
         stack=[]
@@ -11,10 +57,5 @@ class Solution(object):
             stack.append(i)
         # The sum of the array is taken modulo (10^9 + 7) to avoid overflow and adhere to constraints.
         return min_%(10**9 + 7)
-
-
-
-
-
 
         
