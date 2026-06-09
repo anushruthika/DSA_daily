c92f0fdea56024049fae92ead2443d53bc2f0eb5
Anushruthika <anushruthikae@gmail.com>
2026-06-09 09:28:09 +0530

    Update and rename middle_of_LL.py to 002_middle_of_LL.py

--- LinkedList/002_middle_of_LL.py
+++ LinkedList/002_middle_of_LL.py
@@ -1,8 +1,11 @@
+# 876. Middle of the Linked List
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Time complexity: O(n/2) => O(n) space: O(1) => slow fast
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         #not creating and additional variable for fast and using head
