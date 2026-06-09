238ba96b88a599cab722cdc4e47fe0c3611749e5
Anushruthika <anushruthikae@gmail.com>
2026-06-09 10:39:49 +0530

    Update and rename Palindrome_LL.py to 008_palindrome_LL.py

--- LinkedList/008_palindrome_LL.py
+++ LinkedList/008_palindrome_LL.py
@@ -1,9 +1,5 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
+# 234. Palindrome Linked List
+## TC:O(n) SC: O(1)
 class Solution:
     def reverseList(self,head):
         cur=head
