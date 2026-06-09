2a02f8969cf236188cd79d79f9cab965827a3b60
Anushruthika <anushruthikae@gmail.com>
2026-06-09 10:54:48 +0530

    Update and rename odd_even_list.py to 009_odd_even_list.py

--- LinkedList/009_odd_even_list.py
+++ LinkedList/009_odd_even_list.py
@@ -1,8 +1,7 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# 328. Odd Even Linked List
+## TC:O(n) SC:O(1) 
+
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head and head.next:
