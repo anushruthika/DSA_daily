42103d70c5c923132b24132b9e0ec5fac5839704
Anushruthika <anushruthikae@gmail.com>
2026-06-09 10:27:46 +0530

    Update and rename Reverse_LL.py to 007_Reverse_LL.py

--- LinkedList/007_Reverse_LL.py
+++ LinkedList/007_Reverse_LL.py
@@ -1,3 +1,7 @@
+# 206. Reverse Linked List
+
+# TC: O(n) Each node visited exactly once.
+# SC: O(1)
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur = head
