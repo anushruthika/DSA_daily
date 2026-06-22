abdab6e6443836318df235bb452811807e9b8894
Anushruthika <anushruthikae@gmail.com>
2026-06-22 16:40:32 +0530

    Update 006_jump_game_1.py

--- Greedy/006_jump_game_1.py
+++ Greedy/006_jump_game_1.py
@@ -1,5 +1,33 @@
 # 55. Jump Game
 
+############### BRUTE FORCE
+# TC: O(2^n) (exponential)
+# SC: O(n) recursion stack
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        def dfs(ind):
+
+            if ind >= len(nums) - 1:
+                return True
+
+            for jump in range(1, nums[ind] + 1):
+                if dfs(ind + jump):
+                    return True
+
+            return False
+
+        return dfs(0)
+        
+
+
+
+######################## MEMOTISATION DP needed to be added
+
+
+
+
 # TC: O(n)
 # Each index is visited at most once.
 # The while loop increments i from 0 to n-1.
