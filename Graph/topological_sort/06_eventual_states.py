a89bb9a11b8b455abcaa802539b58d8fce3a6ef7
Anushruthika <anushruthikae@gmail.com>
2026-06-23 20:47:25 +0530

    Update 06_eventual_states.py

--- Graph/topological_sort/06_eventual_states.py
+++ Graph/topological_sort/06_eventual_states.py
@@ -2,36 +2,34 @@
 
 # | Solution          | Time             | Space    |
 # | ----------------- | ---------------- | -------- |
-# | Topological sort+ sort(stack) | O(V + E + VlogV) | O(V)     |
-# | Reverse graph DFS | O(V + E)         | O(V + E) |
+# | Topological sort+ sort(stack) | O(V + E) | O(V)  (we dont construct any adj matrix here)   |
+# | Reverse graph DFS | O(V + E)         | O(V + E) (we construct inverted graph adj)|
+
+
 
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         V = len(graph)
-        
-        stack = []
-        path = [False] * V
-        visited = [False] * V
-        
-        def dfs(node):
-            visited[node] = path[node] = True
+        visited = [False]*V
+        path = [False]*V
+        res=[False]*V
+        def isCycle(node):
+            visited[node]=path[node]=True
+            for val in graph[node]:
+                if not visited[val]:
+                    if isCycle(val):
+                        return True
+                elif path[val]:
+                    return True
             
-            for nei in graph[node]:
-                if not visited[nei]:
-                    if not dfs(nei):
-                        return False
-                elif path[nei]:
-                    return False
-            
+            res[node] = True
             path[node] = False
-            stack.append(node)   
-            return True
+            return False
         
         for i in range(V):
             if not visited[i]:
-                dfs(i)
-        
-        return sorted(stack)   # sort because order doesn't matter
+                isCycle(i)
+        return [i for i in range(V) if res[i]]
 
 # inv matrix
 from typing import List
