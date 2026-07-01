77aabe9465c7b06320117e1268cf316a2ab1a757
Anushruthika <anushruthikae@gmail.com>
2026-07-01 11:34:27 +0530

    Update 003_upper_bound.py

--- binary_search/003_upper_bound.py
+++ binary_search/003_upper_bound.py
@@ -2,14 +2,13 @@
 # TC:O(logn) SC:O(n)
 # X > target (if arr[mid]==target: then low=mid+1)
 class Solution:
-    def upperBound(self, arr, target):
-        # code here
+    def upperBound(self, nums, target):
         low = 0
-        high = len(arr)-1
+        high = len(nums)-1
         while low<=high:
-            mid = low+ (high-low)//2
-            if arr[mid]>target:
-                high = mid-1
-            else: 
+            mid = low+(high-low)//2
+            if nums[mid]<=target:
                 low = mid+1
+            else:
+                high=mid -1
         return low
