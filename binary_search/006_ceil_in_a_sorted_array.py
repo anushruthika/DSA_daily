f822cd4bfb6c84f0bfaba841ccc3c8f13af5bd7c
Anushruthika <anushruthikae@gmail.com>
2026-07-02 09:32:59 +0530

    Update 006_ceil_in_a_sorted_array.py

--- binary_search/006_ceil_in_a_sorted_array.py
+++ binary_search/006_ceil_in_a_sorted_array.py
@@ -1,3 +1,7 @@
+# https://www.geeksforgeeks.org/problems/ceil-in-a-sorted-array/1
+# TC: O(log n)
+# SC: O(1)
+# upper bound , if upper bound not found low must have traversed end thus return -1
 class Solution:
     def findCeil(self, arr, x):
         # code here
