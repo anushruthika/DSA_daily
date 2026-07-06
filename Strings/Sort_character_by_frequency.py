05671e415c488c64ee62092decea0265ca26b476
Anushruthika <anushruthikae@gmail.com>
2026-07-06 22:19:28 +0530

    Update Sort_character_by_frequency.py

--- Strings/Sort_character_by_frequency.py
+++ Strings/Sort_character_by_frequency.py
@@ -1,3 +1,6 @@
+# Time Complexity: O(n + k log k)
+# Space Complexity: O(n)
+
 class Solution:
     def frequencySort(self, s: str) -> str:
         d={}
@@ -6,7 +9,22 @@ class Solution:
                 d[i]+=1
             else:
                 d[i]=1
-        st=''
+        # st=''
+        res = []
         for i in sorted(d.items(), key=lambda item: item[1],reverse=True) :
-            st+=i[0]*i[1]
-        return st
+            # String concatination is costly costs O(n) every iteration
+            # st+=i[0]*i[1]
+            res.append(i[0]*i[1])
+        return "".join(res)
+
+# same approach using buitin Counter & one liner:
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        return "".join([i[0]*i[1] for i in sorted(Counter(s).items(), key=lambda x: x[1],reverse=True)])
+
+# Bucket Sort (mention only if asked for a better complexity)
+
+# Time: O(n)
+# Space: O(n)
+
+# YET TO ADD
