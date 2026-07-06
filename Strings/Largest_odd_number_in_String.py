b3e7041c66e05cd480025d0beca6213507089e4c
Anushruthika <anushruthikae@gmail.com>
2026-07-06 18:50:59 +0530

    Update Largest_odd_number_in_String.py

--- Strings/Largest_odd_number_in_String.py
+++ Strings/Largest_odd_number_in_String.py
@@ -1,7 +1,11 @@
-import math
+# Time Complexity: O(n)
+# Space Complexity: O(n)(due to creating a new substring for the output) string slicing
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        for i in range(len(num)-1,-1,-1):
+        i = len(num)-1
+
+        while i>=0:
             if int(num[i])%2!=0:
                 return num[:i+1]
+            i-=1
         return ""
