0961105dbb2beda24977a9a6afd4764217ebcd99
Anushruthika <anushruthikae@gmail.com>
2026-07-08 09:26:06 +0530

    Update roman_to_integer.py

--- Strings/roman_to_integer.py
+++ Strings/roman_to_integer.py
@@ -1,13 +1,22 @@
+# 13. Roman to Integer
+
+# Time Complexity: O(n)
+# - We traverse the string once, and each dictionary lookup is O(1).
+
+# Space Complexity: O(1)
+# - The dictionary has a fixed size of 7 entries, so it uses constant space.
+
 class Solution:
     def romanToInt(self, s: str) -> int:
-        sum_=0
-        d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
-        i=0
-        while i < len(s):
-            if s[i:i+2] in d:
-                sum_+=d[s[i:i+2]]
-                i=i+2    
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        i = 0
+        ans = 0
+        n = len(s)
+        while i<n:
+            if i+1<n and roman[s[i+1]]>roman[s[i]]:
+                ans+=roman[s[i+1]]-roman[s[i]]
+                i=i+1
             else:
-                sum_+=d[s[i]]
-                i+=1
-        return sum_
+                ans+=roman[s[i]]
+            i+=1
+        return ans
