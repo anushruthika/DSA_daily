5f5b8c6e1e331694688be4eca3da6afcd749cd38
Anushruthika <anushruthikae@gmail.com>
2026-07-08 09:14:53 +0530

    Update maximum_nesting_depth_of_the_parentheses.py

--- Strings/maximum_nesting_depth_of_the_parentheses.py
+++ Strings/maximum_nesting_depth_of_the_parentheses.py
@@ -1,13 +1,22 @@
+# 1614. Maximum Nesting Depth of the Parentheses
+
+# Time Complexity
+# You traverse the string once: O(n)
+# Space Complexity
+# You only use two integer variables: O(1)
 class Solution:
     def maxDepth(self, s: str) -> int:
-        count=0
-        m=0
+        max_count = 0
+        count = 0
         for i in s:
-            if i =='(':
+            if i == '(':
                 count+=1
-                if count>m:
-                    m=count
-            elif i==')':
-                count-=1
-        return m
+                max_count = max(count,max_count)
+            elif i == ')':
+                count -=1
+                if count<0:
+                    # invalid string
+                    return -1
+        return max_count
+
         
