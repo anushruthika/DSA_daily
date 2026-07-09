69f2a3ffc16f089b8073431089b1937166691606
Anushruthika <anushruthikae@gmail.com>
2026-07-09 10:45:50 +0530

    Update and rename Longest_palindromic_substring.py to 013_longest_palindromic_substring.py

--- Strings/013_longest_palindromic_substring.py
+++ Strings/013_longest_palindromic_substring.py
@@ -1,3 +1,5 @@
+# 5. Longest Palindromic Substring
+
 # | Algorithm            | Time    | Space   | Category                              |
 # | -------------------- | ------- | ------- | ------------------------------------- |
 # | Brute Force          | `O(n³)` | `O(1)`  | Enumeration                           |
@@ -22,3 +24,22 @@ class Solution:
             i+=1
         return ans
 
+# Expand around center
+class Solution:
+    def expand_from_center(self,left,right,s):
+        while left>=0 and right<len(s) and s[left] == s[right]:
+            left -=1
+            right+=1
+        return s[left+1:right]
+    def longestPalindrome(self, s: str) -> str:
+        if len(s)<=1:
+            return s
+        max_str = ""
+        for i in range(len(s)):
+            odd = self.expand_from_center(i,i,s)
+            even = self.expand_from_center(i,i+1,s)
+            if len(max_str) < len(odd):
+                max_str = odd
+            if len(max_str) < len(even):
+                max_str = even
+        return max_str
