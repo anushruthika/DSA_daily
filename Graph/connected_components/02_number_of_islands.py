017e9da44a35ad09a861818998afb612bb3c7482
Anushruthika <anushruthikae@gmail.com>
2026-07-23 16:09:22 +0530

    Update 02_number_of_islands.py

--- Graph/connected_components/02_number_of_islands.py
+++ Graph/connected_components/02_number_of_islands.py
@@ -4,6 +4,58 @@
 
 # Space: O(m * n) => queue can hold all cells in worst case (one big island)
 
+
+########## DSU
+from collections import deque
+class Solution:
+    class DSU:
+        def __init__(self,n):
+            self.parent = [i for i in range(n)]
+            self.rank = [0]*n
+        def find(self,x):
+            if self.parent[x]!= x:
+                self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+        def union(self,u,v):
+            pu = self.find(u)
+            pv = self.find(v)
+            if pu == pv:
+                return False
+            if self.rank[pu]>self.rank[pv]:
+                self.parent[pv] = self.parent[pu]
+            elif self.rank[pv]>self.rank[pu]:
+                self.parent[pu] = self.parent[pv]
+            else:
+                self.parent[pv] = self.parent[pu]
+                self.rank[pu]+=1
+            return True
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        lr = len(grid)
+        lc = len(grid[0])
+        dsu = self.DSU(lr*lc)
+        directions = [(0, 1), (1, 0),(-1,0),(0,-1)]  
+        for i in range(lr):
+            for j in range(lc):
+                if grid[i][j] == '1':
+                    node = i * lc + j
+                    for dr, dc in directions:
+                        nr,nc=dr+i,dc+j
+                        if 0<=nr<lr and 0<=nc<lc and grid[nr][nc]=='1':
+                            con = (nr)*lc +(nc)
+                            dsu.union(node,con)
+        res = set()
+        for i in range(lr):
+            for j in range(lc):
+                if grid[i][j] == '1':
+                    node = i * lc + j
+                    res.add(dsu.find(node))
+        return len(res)
+
+
+
+
+########## BFS
 from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
