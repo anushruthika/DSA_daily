7727ac506a637593840d7aa001acf4c5d7fc3b4c
Anushruthika <anushruthikae@gmail.com>
2026-07-23 15:18:45 +0530

    Update 05_accounts_merge.py

--- Graph/connected_components/05_accounts_merge.py
+++ Graph/connected_components/05_accounts_merge.py
@@ -25,53 +25,60 @@
 
 from collections import defaultdict
 
-class DisjointSet:
-
-    def __init__(self, n):
-        self.parent = [i for i in range(n)]
-        self.rank = [0] * n
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, u, v):
-
-        pu = self.find(u)
-        pv = self.find(v)
-
-        if pu == pv:
-            return
-
-        if self.rank[pu] < self.rank[pv]:
-            self.parent[pu] = pv
-
-        elif self.rank[pv] < self.rank[pu]:
-            self.parent[pv] = pu
-
-        else:
-            self.parent[pv] = pu
-            self.rank[pu] += 1
-
-
+from collections import defaultdict
 class Solution:
-    def accountsMerge(self, accounts):
+    class DSU:
+        def __init__(self,n):
+            self.parent = [i for i in range(n)]
+            self.rank = [0]*n
+        def find(self,x):
+            if self.parent[x] != x:
+                self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+        def union(self,u,v):
+            pu = self.find(u)
+            pv = self.find(v)
+            if pu == pv:
+                return False
+            if self.rank[pu]>self.rank[pv]:
+                self.parent[pv] = self.parent[pu]
+            elif self.rank[pv] >self.rank[pu]:
+                self.parent[pu] = self.parent[pv]
+            else:
+                self.parent[pv] = self.parent[pu]
+                self.rank[pu]+=1
+            return True
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         n = len(accounts)
-        dsu = DisjointSet(n)
+        dsu = self.DSU(n)
+        unique_emails = defaultdict(int)
 
-        uniq_emails = defaultdict(list)
-        for i,l in enumerate(accounts):
-            for acc in l[1:]:
-                if acc not in uniq_emails:
-                    uniq_emails[acc] = i
+        # E     : lets take=> n*m(emails in one account)= E (total number of emails)
+        # O(n*m(emails in one account)*α(N)) 
+
+        # O(E*α(N))
+        # {"johnsmith@mail.com": 0,"john_newyork@mail.com":0,"john00@mail.com":1,"mary@mail.com":2,"johnnybravo@mail.com":3 }
+        for i,account in enumerate(accounts):
+            for email in account[1:]:
+                if email not in unique_emails:
+                    unique_emails[email] = i
                 else:
-                    dsu.union(uniq_emails[acc],i)
-        merged = defaultdict(list)
+                    dsu.union(unique_emails[email],i)
 
-        for email,node in uniq_emails.items():
-            merged[dsu.find(node)].append(email)
+        merged_list = defaultdict(list)
+        # {0:["johnsmith@mail.com","john_newyork@mail.com"],
+        # 1: ["john00@mail.com"], 2:["mary@mail.com"], 3:["johnnybravo@mail.com"]}
+        
+        # O(E*α(N))
+        for email,node in unique_emails.items():                     # <- O(E) : runs E times 
+            merged_list[dsu.find(node)].append(email)                # <- O(α(N)) : complexity of dsu.find(node)
+
+        # O(ElogE all the emails partioned go through sorting)
         res = []
-        for name,emails in merged.items():
-            res.append([accounts[name][0]]+sorted(emails))
+        for node,email_list in merged_list.items():
+            res.append([accounts[node][0]]+sorted(email_list))
         return res
+        # TC: O(E*α(N) + ElogE)
+        # SC: space for DSU -> O(n) -> parent,rank array
+        # O(E)-> unique_emails O(E) -> merged_list o(E) -> res
+        # ans: O(n+E)
