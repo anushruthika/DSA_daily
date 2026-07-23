2ee8514c1774a6cd23f19a87570c2201a4b6f4f4
Anushruthika <anushruthikae@gmail.com>
2026-07-23 19:08:51 +0530

    Update 01_cycle_undirected_graph_general.py

--- Graph/topological_sort/01_cycle_undirected_graph_general.py
+++ Graph/topological_sort/01_cycle_undirected_graph_general.py
@@ -1,6 +1,53 @@
 # Time: O(V + E) => building adjacency list takes O(E), and DFS visits each node and edge once
 # Space: O(V + E) => adjacency list stores all edges + visited array + recursion stack (worst case O(V))
 
+# time is O(V+E) and not O(V*E)
+			# lets take 
+			# graph : 0->1->2 and 3->4 
+			# V: 5 E: 3 expected TC: O(V+E) = O(8)
+			# base pseudo code:
+			# iterate i from (0-1-2-3-4):
+			#       if not visited :
+			#              isCycle()
+			# Proof:
+			# i = 0 
+			# isCycle(0)-> explores 1&2 TC: O(2)
+			# total : O(3) traverses 1-2-3
+			
+			# i=1
+			# already visited
+			# O(1)
+			# i=2
+			# already visited
+			# O(1)
+			
+			# i=3
+			# isCycle(3) - to visit 4
+			# O(2) as it traverses 3-4
+			
+			# i=4
+			# already visited
+			# O(1)
+			
+			# sum of all is O(8)
+			# Hence proved. 
+
+
+# SPACE complexity:
+			# adj =
+			
+			# 0 : [1]
+			# 1 : [0,2]
+			# 2 : [1]
+			# 3 : [4]
+			# 4 : [3]
+
+			# so 5 keys - 5 Vertex
+			# then i have 6 values. meaning we have 3 edges. for a undirected graph it is 2*E = 6 edges
+			# space for this is O(11)
+			# O(V+2E) = O(V+E)
+
+
 # https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
 
 
