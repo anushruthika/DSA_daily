07b20ed4634615f9169e8bb3ad8774ed9f6eaac5
Anushruthika <anushruthikae@gmail.com>
2026-07-27 18:27:11 +0530

    Update 004_longest_common_prefix.py

--- Strings/004_longest_common_prefix.py
+++ Strings/004_longest_common_prefix.py
@@ -4,26 +4,27 @@
 # Auxiliary Space: O(1)
 # Space Complexity (including returned string): O(m)
 
+# O(n*m) m : denotes the length of the longest string
+# case: ["test","test","test"]
+# case: [] output = ""
+# case : ["run","bun"] output = "" no match 
+# case : ["test"] output = "test"
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        i = 0
-        n = len(strs)
-        res = len(strs[0])
-        while i<n-1:
-            s1,s2 = strs[i],strs[i+1]
-            n1,n2 = len(s1),len(s2)
-            count = 0
-            j=0
-            k=0
-            while j<n1 and k<n2:
-                if s1[j] == s2[k] :
-                    count+=1
-                else:
-                    break
+        length_of_strs = len(strs)
+        if length_of_strs==0:
+            return ""
+        if length_of_strs== 1:
+            return strs[0]
+        prefix = strs[0]
+        # iteration variable
+        i = 1
+        while i<length_of_strs:
+            j = 0
+            while j<len(strs[i]) and j<len(prefix) and strs[i][j] == prefix[j]:
                 j+=1
-                k+=1
-            res = min(count,res)
+            prefix = prefix[:j]
             i+=1
-        return strs[0][:res]
-
- 
+            if prefix == "":
+                return ""
+        return prefix
