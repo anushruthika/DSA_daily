0913e34dd6d0c9141e11acd736bcb29939171755
Anushruthika <anushruthikae@gmail.com>
2026-08-27 14:46:09 +0530

    Update 25_count_node.py

--- Trees/25_count_node.py
+++ Trees/25_count_node.py
@@ -1,4 +1,42 @@
 # 222. Count Complete Tree Nodes
+
+# TC: O(log**2n)
+# At each recursive level, I calculate the left and right heights in O(log n). 
+# The recursion goes at most O(log n) levels because the tree is complete. Therefore the total time complexity is O(log² n).
+# Space: O(logn)​
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        def left_count(cur):
+            count = 0
+            while cur:
+                cur = cur.left
+                count+=1
+            return count
+        def right_count(cur):
+            count = 0
+            while cur:
+                cur = cur.right
+                count+=1
+            return count
+        lh = left_count(root)
+        rh = right_count(root)
+        if lh == rh:
+            return 2**lh -1
+        return 1+ self.countNodes(root.left)+ self.countNodes(root.right)
+                    
+
+                    
+
+            
+
 # Time: O(n) => all nodes traversed once
 # Space: O(h) (worst O(n) => skewed Tree, best O(log n) => balanced tree)
 
