55b820d38069a248a6f97bf102b777141b2d40f0
Anushruthika <anushruthikae@gmail.com>
2026-09-02 00:10:21 +0530

    Update 14_knight_walk.py

--- Graph/shortest_path/14_knight_walk.py
+++ Graph/shortest_path/14_knight_walk.py
@@ -5,34 +5,18 @@
 
 from collections import deque
 class Solution:
-    def minStepToReachTarget(self, knightPos, targetPos, n):
-        # Convert to 0-based indexing
-        sr, sc = knightPos[0] - 1, knightPos[1] - 1
-        tr, tc = targetPos[0] - 1, targetPos[1] - 1
-        # Knight moves
-        directions = [
-            (-2, -1), (-2, 1),
-            (-1, -2), (-1, 2),
-            (1, -2), (1, 2),
-            (2, -1), (2, 1)
-        ]
-        q = deque()
-        q.append((sr, sc, 0))
-        visited = set()
-        visited.add((sr, sc))
-        while q:
-            r, c, steps = q.popleft()
-            # Reached target
-            if (r, c) == (tr, tc):
-                return steps
-            # Explore all knight moves
-            for dr, dc in directions:
-                nr = r + dr
-                nc = c + dc
-                # Valid cell
-                if 0 <= nr < n and 0 <= nc < n:
-                    if (nr, nc) not in visited:
-                        visited.add((nr, nc))
-                        q.append((nr, nc, steps + 1))
-
-        return -1
+	def minStepToReachTarget(self, knightPos, targetPos, n):
+		directions = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(-1,2),(1,-2),(-1,-2)]
+		queue = deque([(knightPos[0]-1,knightPos[1]-1,0)])
+		seen = set()
+		while queue:
+		    r,c,steps = queue.popleft()
+		    if r == targetPos[0]-1 and c == targetPos[1]-1:
+		        return steps
+		    for dr,dc in directions:
+		        nr,nc = dr+r,dc+c
+		        if 0<=nr<n and 0<=nc<n and (nr,nc) not in seen:
+		            seen.add((nr,nc))
+		            queue.append((nr,nc,steps+1))
+		return -1
+		
