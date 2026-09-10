58070a3169455fc27fbf77f7f000e27e526d6b82
Anushruthika <anushruthikae@gmail.com>
2026-09-10 09:43:22 +0530

    Update 001_check_if_arr_max_heap.py

--- heap/001_check_if_arr_max_heap.py
+++ heap/001_check_if_arr_max_heap.py
@@ -1,4 +1,30 @@
 # https://www.geeksforgeeks.org/problems/does-array-represent-heap4345/1
+
+# TC:o(n) SC: O(n)
+from collections import deque
+class Solution:
+    def isMaxHeap(self, arr):
+        n = len(arr)
+        if n>0:
+            queue = deque([0])
+            while queue:
+                ind = queue.popleft()
+                node = arr[ind]
+                if 2*ind+1>=n:
+                    continue
+                left = arr[2*ind+1]
+                if node<left:
+                    return False
+                queue.append(2*ind+1)
+                if 2*ind+2>=n:
+                    continue
+                right = arr[2*ind+2]
+                if node<right:
+                    return False
+                queue.append(2*ind+2)
+        return True
+        
+        
 # TC:o(nlogn) SC: O(n)
 import heapq
 class Solution:
