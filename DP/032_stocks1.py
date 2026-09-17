aa2ecf9026ae84715f3671b09a7a0e1797b0ed14
Anushruthika <anushruthikae@gmail.com>
2026-09-17 09:42:11 +0530

    Update 032_stocks1.py

--- DP/032_stocks1.py
+++ DP/032_stocks1.py
@@ -1,7 +1,15 @@
 # 121. Best Time to Buy and Sell Stock
 
+# buy/sell only once within the window
+
+# decreasing sells. maximum you can buy and sell on same day
+# [7,6,5,4,3,2] ans = 0
+# buy and sell on same day
+# [1] ans = 0
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # min profit is zero. ,because buy and sell on same day
         ans_profit = 0
         mini = prices[0]
         n = len(prices)
