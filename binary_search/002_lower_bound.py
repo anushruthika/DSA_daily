dfbc592ebab1ac96186f77dd6fd668871a93ebe0
Anushruthika <anushruthikae@gmail.com>
2026-09-17 17:57:19 +0530

    Update 002_lower_bound.py

--- binary_search/002_lower_bound.py
+++ binary_search/002_lower_bound.py
@@ -1,4 +1,15 @@
 # https://www.geeksforgeeks.org/problems/implement-lower-bound/1
+
+# lower bound: smallest index such that arr[ind]>=x
+# eg: 
+# [19,19,19] x = 19 ans: 0
+
+# Upper bound: smallest index such that arr[ind]>x
+# eg:
+# [19,19,19] x = 19 ans = 3
+# [19,19,19,20] x = 19 ans = 3
+
+
 # X >= target (if arr[mid]==target: then high=mid-1)
 # TC:O(logn) SC:O(1)
 class Solution:
