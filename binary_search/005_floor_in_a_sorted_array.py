efd206ce0437eead1e2a0705fe4e7cafefdce488
Anushruthika <anushruthikae@gmail.com>
2026-09-21 11:57:56 +0530

    Update 005_floor_in_a_sorted_array.py

--- binary_search/005_floor_in_a_sorted_array.py
+++ binary_search/005_floor_in_a_sorted_array.py
@@ -1,6 +1,15 @@
 # TC: O(log n)
 # SC: O(1)
 # same upper bound but return high
+
+# floor == Upper bound -1 (floor == ans is element before strictly greater than x therefore up-1)
+#  upper bound -1: example: 
+[11,11,25]     x: 12 upbound : 25 floor: upbound-1: 11
+[11,11,25]     x: 11 upbound: 25 (strictly greater) upbound-1: 11
+
+# Ceil == lower bound (greater or equal == definition of lb)
+[9,11,11,25] x:12 lb: 25 ceil: 25 
+[9,11,11,25] x:11 lb: 11 ceil: 11
 class Solution:
     def findFloor(self, arr, x):
         low = 0
