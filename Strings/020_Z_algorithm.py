51ee19ea7ed76248911afa6aa941ae14a83514f6
Anushruthika <anushruthikae@gmail.com>
2026-09-22 20:45:20 +0530

    Update 020_Z_algorithm.py

--- Strings/020_Z_algorithm.py
+++ Strings/020_Z_algorithm.py
@@ -1,6 +1,33 @@
 # https://www.youtube.com/watch?v=BhxJiqShYcQ
 # https://www.naukri.com/code360/problems/z-algorithm_1112619
 
+
+def zAlgorithm(s, p, n, m):
+    if n<m:
+        return -1
+    zstring = p+'$'+s
+    z = [0]*(m+n+1)
+    # print(z,zstring)
+    
+    i = 0
+    # ans = []
+    count = 0
+    for i in range(m+1,m+n+1):
+        ind = i
+        j = 0
+        while ind<m+n+1 and j<m and zstring[ind] == zstring[j]:
+            j+=1
+            ind+=1
+        z[i] = ind-i
+        if z[i] == m:
+            count+=1
+            # ans.append(i-m-1)
+    # print(z)
+    return count
+
+
+            
+
 # Time Complexity: O(n + m)
 # Space Complexity: O(n + m)
 
